f972daf31a87e30e612d1ce8a02b543d54b90ca2
evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>
2026-01-06 22:09:47 +0600

    Update clone_kernel.sh
    Signed-off-by: evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>

--- ci/clone_kernel.sh
+++ ci/clone_kernel.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-KERNEL_SOURCE="${1:?kernel_source required}"
-KERNEL_BRANCH="${2:?kernel_branch required}"
+SRC="${1:?kernel_source required}"
+BRANCH="${2:?kernel_branch required}"
 
 rm -rf kernel
-git clone --depth=1 -b "${KERNEL_BRANCH}" "${KERNEL_SOURCE}" kernel
+git clone --depth=1 -b "$BRANCH" "$SRC" kernel
