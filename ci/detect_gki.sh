646ae617fa918f6f78d8b3ade8edc28a5a84042d
evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>
2026-01-07 19:06:25 +0600

    Update detect_gki.sh
    Signed-off-by: evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>

--- ci/detect_gki.sh
+++ ci/detect_gki.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env bash
-set -euo pipefail
 
-if [ -f kernel/out/.config ] && grep -q '^CONFIG_GKI=y' kernel/out/.config; then
-  echo "KERNEL_TYPE=GKI" >> "$GITHUB_ENV"
-else
-  echo "KERNEL_TYPE=NON-GKI" >> "$GITHUB_ENV"
-fi
