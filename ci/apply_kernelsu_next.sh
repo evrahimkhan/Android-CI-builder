5e739c3d17859fb8f5cf060c9e2a2fe3c9e624f2
evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>
2026-01-11 14:58:59 +0600

    Delete ci/apply_kernelsu_next.sh
    Signed-off-by: evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>

--- ci/apply_kernelsu_next.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd kernel
-curl -LSs https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh | bash -
-git status --porcelain || true
