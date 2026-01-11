c561c5acca57f03ba6efbf57d56f21855bce9660
evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>
2026-01-11 15:04:01 +0600

    Update ensure_anykernel_core.sh
    Signed-off-by: evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>

--- ci/ensure_anykernel_core.sh
+++ ci/ensure_anykernel_core.sh
@@ -5,7 +5,7 @@ test -f anykernel/anykernel.sh || { echo "Missing anykernel/anykernel.sh"; exit 
 
 if [ ! -f anykernel/tools/ak3-core.sh ]; then
   rm -rf anykernel_upstream
-  git clone --depth=1 https://github.com/osm0sis/AnyKernel3 anykernel_upstream
+  git clone --depth=1 https://github.com/osm0sis/AnyKernel3  anykernel_upstream
   rsync -a --exclude 'anykernel.sh' anykernel_upstream/ anykernel/
   rm -rf anykernel_upstream
 fi
